22bd12823bc52dfc6b61e24543fc38df084f016d
Facundo Bravo <facundobravot@gmail.com>
2022-10-07 23:28:25 -0300

    0.0.1
    Rubik folder upload

---
+++ Assets/CreateParent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateParent : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void GetCubes()
+    {
+        //use raycast to get the 9 cubes (3 raycast should pierce 3 cubes each)
+
+        //raycast 1 
+        //raycast 2 
+        //raycast 3
+        
+        //en un for:
+        //raycastAll()
+    }
+
+    public void CreateCubeArray()
+    {
+
+    }
+
+    public void CreateAnchorParent()
+    {
+
+    }
+
+}
